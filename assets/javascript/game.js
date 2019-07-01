450a1ba0083ab50042c34fe4517b3f06de61438b
Eliza Irwin <elizairwin@gmail.com>
2019-07-01 13:37:29 -0400

    JavaScript first draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,74 @@
-var lettersGuessed = []
-var remainingGuesses = 10;
 var wins = 0;
+var losses = 0;
+var remainingGuesses = 10;
+var lettersGuessed = [];
 
+var computerGuess = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+];
+
+//if the player guesses the same as the computer, the below happens
+if (userGuess === computerGuess) {
+    alert("You have ESP!");
+    wins++;
+    lettersGuessed.push(userGuess);
+    remainingGuesses--;
+}
+
+//if the player guesses different than the computer, the below happens
+if (userGuess !== computerGuess) {
+    alert("Not a match!");
+    losses++;
+    lettersGuessed.push(userGuess);
+    remainingGuesses--;
+}
+
+//if the player has 0 chances left, the below happens
+if (remainingGuesses === 0) {
+    alert("You're not a psychic!")
+    location.reload();
+}
+
+//if the player loses 10 times, the below happens 
+else if (losses === 10) {
+    alert("And that's a wrap! 10 guesses and you're out.");
+    location.reload();
+}
+
+//if the player wins all 10 times he or she plays, the below happens
+
+else if (wins === 10) {
+    alert("10 times is a charm. You're super psychic!")
+    location.reload();
+}
+
+//error portion below - i.e. the player doesn't push correct key
+
+else {
+    alert("Sorry, not a valid guess!")
+}
